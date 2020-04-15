108d63239486fc2fde829f314dfc7fa2c558f26a
KC Kai <kirstenckai@gmail.com>
2020-04-15 03:29:58 -0400

    Add Header component with conditional rendering of Show and Empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,9 +2,25 @@ import React from "react";
 
 import "./styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+// All Appointment components will render a Header that takes in a time prop.
+// If props.interview is truthy (an interview object) the Appointment will render the <Show /> component, else it should render the <Empty /> component.
+
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      { props.interview ?
+        <Show
+          student={props.student}
+          interviewer={props.interviewer}
+        />
+        : <Empty />
+      }
+    </article>
   )
 
 }
