61d7dc50aa095b863707f7c9302a063bfdb019c8
KC Kai <kirstenckai@gmail.com>
2020-04-18 13:37:05 -0400

    Add logic for useVisualMode function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,7 +11,15 @@ import useVisualMode from "../../hooks/useVisualMode";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 
+// The next step is to use our useVisualMode Hook. The same logic as before determines the initial mode. When props.interview contains a value, then we want to pass useVisualMode the SHOW mode, if it is empty then we should pass EMPTY.
+
+
 export default function Appointment(props) {
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
@@ -24,5 +32,4 @@ export default function Appointment(props) {
       }
     </article>
   )
-
 }
