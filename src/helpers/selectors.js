c571cd3d000ecb1cf02c2b97547f2e8387f2e8b3
KC Kai <kirstenckai@gmail.com>
2020-04-18 09:27:12 -0400

    Implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -5,3 +5,15 @@ export function getAppointmentsForDay(state, day) {
    const filteredAppointments = filteredDays[0].appointments.map(id =>  state.appointments[id])
    return filteredAppointments
 }
+
+export function getInterview(state, interview) {
+  if (!interview) return null
+  let { student } = interview
+  const interviewObj = {
+    student,
+    interviewer: {
+      ...state.interviewers[interview.interviewer]
+    }
+  }
+  return interviewObj
+}
