609cd87c78a673d3b89d53965266fe27771c2c30
KC Kai <kirstenckai@gmail.com>
2020-04-20 15:16:48 -0400

    Update onChange to correct prop

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -34,12 +34,16 @@ export default function Form(props) {
             placeholder="Enter Student Name"
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(setInterviewer)}/>
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          value={interviewer} 
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.save}>Save</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
